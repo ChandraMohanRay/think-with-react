7686cd76217e605719c7963b6a863dcda863155c
ChandraMohanRay <cmohan932@hotmail.com>
2021-10-16 21:00:47 +0530

    1st 2nd task complete

---
+++ app.js
@@ -0,0 +1,14 @@
+// const container = document.getElementById('react-container');
+// ReactDOM.render("Hello! Welcome to React",container);
+
+
+// Functional Components :
+
+const container1 = ()=>{
+    return
+    React.createElement('div',null,'welcome to react games',
+    React.createElement('div',null,'lets rock n roll'));
+}
+
+const container1 = document.getElementById('react-container');
+ReactDOM.render(React.createElement(Container),container);
